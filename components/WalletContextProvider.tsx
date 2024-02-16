6a19190bc9fdd899e81b61f3eda1fd2eda4988b9
aleynaalangil <aleynaalangil@gmail.com>
2024-02-16 12:06:46 +0300

    cluster url changed

--- components/WalletContextProvider.tsx
+++ components/WalletContextProvider.tsx
@@ -12,11 +12,11 @@ const WalletContextProvider: FC<{ children: ReactNode }> = ({children}) => {
     const wallets = [new PhantomWalletAdapter(),
         new GlowWalletAdapter()];
 
-    const endpoint = web3.clusterApiUrl("mainnet-beta");
-
+    const endpoint = new web3.Connection("https://palpable-wiser-morning.solana-mainnet.quiknode.pro/458e1acd4eac134013635891edb37ca0076383af/");
+    const strEnd = endpoint.rpcEndpoint
     return (
 
-        <ConnectionProvider endpoint={endpoint} >
+        <ConnectionProvider endpoint={strEnd} >
             <WalletProvider wallets={wallets}>
                 <WalletModalProvider>
                     {children}
