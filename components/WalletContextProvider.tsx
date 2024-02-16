28adfb324239a7916be587b6eacdf7612c89881f
aleynaalangil <aleynaalangil@gmail.com>
2024-02-16 11:27:03 +0300

    update field change

--- components/WalletContextProvider.tsx
+++ components/WalletContextProvider.tsx
@@ -6,7 +6,6 @@ import {
 import {WalletModalProvider} from "@solana/wallet-adapter-react-ui";
 import * as web3 from "@solana/web3.js";
 import {GlowWalletAdapter, PhantomWalletAdapter} from "@solana/wallet-adapter-wallets";
-desc
 require("@solana/wallet-adapter-react-ui/styles.css");
 
 const WalletContextProvider: FC<{ children: ReactNode }> = ({children}) => {
